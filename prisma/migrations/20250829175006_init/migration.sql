a85c942f6f032e29c92b5666243a43227a27a52b
Manuel <manuknz96@gmail.com>
2025-08-29 14:57:54 -0300

    feat: primera migracion

---
+++ prisma/migrations/20250829175006_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "avatarUrl" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."FavoriteCity" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "countryCode" TEXT,
+    "lat" DOUBLE PRECISION,
+    "lon" DOUBLE PRECISION,
+    "notes" TEXT,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FavoriteCity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE INDEX "FavoriteCity_userId_idx" ON "public"."FavoriteCity"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FavoriteCity_userId_name_countryCode_key" ON "public"."FavoriteCity"("userId", "name", "countryCode");
+
+-- AddForeignKey
+ALTER TABLE "public"."FavoriteCity" ADD CONSTRAINT "FavoriteCity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
